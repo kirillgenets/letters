ca3b4fc2d6bc44b019110f56b7b9f26c7408536d
kirillgenets <kirill.genets2002@mail.ru>
2019-11-17 09:17:50 +0300

    downloaded chapter 4

--- src/App.js
+++ src/App.js
@@ -1,173 +1,108 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
 import PropTypes from 'prop-types';
+import parseLinkHeader from 'parse-link-header';
+import orderBy from 'lodash/orderBy';
 
-const node = document.getElementById('root');
+import ErrorMessage from './components/error/Error';
+import Nav from './components/nav/navbar';
+import Loader from './components/Loader';
 
-const data = {
-  post: {
-    id: 123,
-    content:
-      'What we hope ever to do with ease, we must first learn to do with diligence. — Samuel Johnson',
-    user: 'Mark Thomas'
-  },
-  comments: [
-    {
-      id: 0,
-      user: 'David',
-      content: 'such. win.'
-    },
-    {
-      id: 1,
-      user: 'Haley',
-      content: 'Love it.'
-    },
-    {
-      id: 2,
-      user: 'Peter',
-      content: 'Who was Samuel Johnson?'
-    },
-    {
-      id: 3,
-      user: 'Mitchell',
-      content: '@Peter get off Letters and do your homework'
-    },
-    {
-      id: 4,
-      user: 'Peter',
-      content: '@mitchell ok :P'
+import * as API from './shared/http';
+import Ad from './components/ad/Ad';
+import Post from './components/post/Post';
+import Welcome from './components/welcome/Welcome';
+
+/**
+ * The app component serves as a root for the project and renders either children,
+ * the error state, or a loading state
+ * @method App
+ * @module letters/components
+ */
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            loading: false,
+            posts: [],
+            endpoint: `${process.env
+                .ENDPOINT}/posts?_page=1&_sort=date&_order=DESC&_embed=comments&_expand=user&_embed=likes`,
+        };
+        this.getPosts = this.getPosts.bind(this);
     }
-  ]
-};
-class Post extends Component {
-  render() {
-    return (
-      <div className='post'>
-        <h2 className='postAuthor'>{this.props.user}</h2>
-        <span className='postBody'>{this.props.content}</span>
-        {this.props.children}
-      </div>
-    );
-  }
-}
-Post.propTypes = {
-  user: PropTypes.string.isRequired,
-  content: PropTypes.string.isRequired,
-  id: PropTypes.number.isRequired
-};
-
-class Comment extends Component {
-  render() {
-    return (
-      <div className='comment'>
-        <h2 className='commentAuthor'>{this.props.user + ' : '}</h2>
-        <span className='commentContent'>{this.props.content}</span>
-      </div>
-    );
-  }
-}
-
-Comment.propTypes = {
-  id: PropTypes.number.isRequired,
-  content: PropTypes.string.isRequired,
-  user: PropTypes.string.isRequired
-};
-
-class CreateComment extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      content: '',
-      user: ''
+    static propTypes = {
+        children: PropTypes.node,
     };
-    this.handleUserChange = this.handleUserChange.bind(this);
-    this.handleTextChange = this.handleTextChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  handleUserChange(event) {
-    this.setState({
-      user: event.target.value
-    });
-  }
-  handleTextChange(event) {
-    this.setState({
-      content: event.target.value
-    });
-  }
-  handleSubmit(event) {
-    event.preventDefault();
-    this.props.onCommentSubmit({
-      user: this.state.user.trim(),
-      content: this.state.content.trim()
-    });
-    this.setState({
-      user: '',
-      content: ''
-    });
-  }
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className='createComment'>
-        <input
-          value={this.state.user}
-          onChange={this.handleUserChange}
-          placeholder='Your name'
-          type='text'
-        />
-        <input
-          value={this.state.content}
-          onChange={this.handleTextChange}
-          placeholder='Thoughts?'
-          type='text'
-        />
-        <button type='submit'>Post</button>
-      </form>
-    );
-  }
+    componentDidMount() {
+        this.getPosts();
+    }
+    componentDidCatch(err, info) {
+        console.error(err);
+        console.error(info);
+        this.setState(() => ({
+            error: err,
+        }));
+    }
+    getPosts() {
+        API.fetchPosts(this.state.endpoint)
+            .then(res => {
+                return res.json().then(posts => {
+                    const links = parseLinkHeader(res.headers.get('Link'));
+                    this.setState(() => ({
+                        posts: orderBy(this.state.posts.concat(posts), 'date', 'desc'),
+                        endpoint: links.next.url,
+                    }));
+                });
+            })
+            .catch(err => {
+                this.setState(() => ({ error: err }));
+            });
+    }
+    render() {
+        if (this.state.error) {
+            return (
+                <div className="app">
+                    <ErrorMessage error={this.state.error} />
+                </div>
+            );
+        }
+        return (
+            <div className="app">
+                <Nav user={this.props.user} />
+                {this.state.loading ? (
+                    <div className="loading">
+                        <Loader />
+                    </div>
+                ) : (
+                    <div className="home">
+                        <Welcome key="welcome" />
+                        <div>
+                            {this.state.posts.length && (
+                                <div className="posts">
+                                    {this.state.posts.map(({ id }) => {
+                                        return <Post id={id} key={id} user={this.props.user} />;
+                                    })}
+                                </div>
+                            )}
+                            <button className="block" onClick={this.getPosts}>
+                                Load more posts
+                            </button>
+                        </div>
+                        <div>
+                            <Ad
+                                url="https://ifelse.io/book"
+                                imageUrl="/static/assets/ads/ria.png"
+                            />
+                            <Ad
+                                url="https://ifelse.io/book"
+                                imageUrl="/static/assets/ads/orly.jpg"
+                            />
+                        </div>
+                    </div>
+                )}
+            </div>
+        );
+    }
 }
 
-class CommentBox extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      comments: this.props.comments
-    };
-    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
-  }
-  handleCommentSubmit(comment) {
-    const comments = this.state.comments;
-    comment.id = Date.now();
-    const newComments = comments.concat([comment]);
-    this.setState({
-      comments: newComments
-    });
-  }
-  render() {
-    return (
-      <div className='commentBox'>
-        <Post
-          id={this.props.post.id}
-          content={this.props.post.content}
-          user={this.props.post.user}
-        />
-        {this.state.comments.map(function(comment) {
-          return (
-            <Comment
-              key={comment.id}
-              content={comment.content}
-              user={comment.user}
-            />
-          );
-        })}
-        <CreateComment onCommentSubmit={this.handleCommentSubmit} />
-      </div>
-    );
-  }
-}
-
-CommentBox.propTypes = {
-  post: PropTypes.object,
-  comments: PropTypes.arrayOf(PropTypes.object)
-};
-
-render(<CommentBox comments={data.comments} post={data.post} />, node);
+export default App;
